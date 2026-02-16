4762018fa20599e7d9271eb0e2592a50993ff568
omonillo542-create <omonillo542@gmail.com>
2026-02-16 15:52:31 +0500

    Add configuration settings file

---
+++ config.py
@@ -0,0 +1,20 @@
+# Configuration Settings
+
+DATABASE = {
+    'HOST': 'localhost',
+    'USER': 'your_db_user',
+    'PASSWORD': 'your_db_password',
+    'NAME': 'your_db_name'
+}
+
+SECRET_KEY = 'your_secret_key'
+
+PAYMENT_SETTINGS = {
+    'API_KEY': 'your_payment_api_key',
+    'CURRENCY': 'USD'
+}
+
+API_KEYS = {
+    'SERVICE_1': 'your_service_1_api_key',
+    'SERVICE_2': 'your_service_2_api_key'
+}
