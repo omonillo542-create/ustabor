c163e8c02244546c4bc907aafa3ba890f5f16eb3
omonillo542-create <omonillo542@gmail.com>
2026-02-16 15:56:13 +0500

    Add PaymentService class with methods for payment handling

---
+++ payment_service.py
@@ -0,0 +1,24 @@
+class PaymentService:
+    def calculate_commission(self):
+        # Logic for calculating commission
+        pass
+
+    def create_payment(self):
+        # Logic for creating a payment
+        pass
+
+    def complete_payment(self):
+        # Logic for completing a payment
+        pass
+
+    def failed_payment(self):
+        # Logic for handling failed payment
+        pass
+
+    def get_master_wallet(self):
+        # Logic to get master wallet
+        pass
+
+    def refund_payment(self):
+        # Logic for refunding a payment
+        pass
